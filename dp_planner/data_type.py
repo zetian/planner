29ba90a5b73f86a18953098b8862bc5f8642e09e
zetian <zetianzzt@gmail.com>
2019-04-15 02:12:59 -0700

    add collision check and more

--- dp_planner/data_type.py
+++ dp_planner/data_type.py
@@ -1,18 +1,41 @@
+import math
 class Pose_t:
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.time = 0
         self.heading = 0
         self.arc_len = 0
         self.curvature = 0
 
+class FrenetPose_t:
+    def __init__(self, s = 0, l = 0, t = 0):
+        self.s = s
+        self.l = l
+        self.t = t
+        
+    def __key(self):
+        return (self.s, self.l, self.t)
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __eq__(self, other):
+        return isinstance(self, type(other)) and self.__key() == other.__key()
+
 class Path_t:
     def __init__(self):
         self.path = []
-
 
 class Obstacle:
     def __init__(self):
         self.pose = Pose_t()
         self.length = 0
         self.width = 0
+        self.radius = 0
+        self.time = 0
+        self.moving = True
+        self.predict_pose = []
+
+def dist(pose_1, pose_2):
+    return math.hypot(pose_1.x - pose_2.x, pose_1.y - pose_2.y)
--- dp_planner/dp_node.py
+++ dp_planner/dp_node.py
@@ -5,6 +5,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../math/")
 from polynomial_curve1d import *
+from data_type import *
 
 class PathCost:
     def __init__(self):
@@ -39,17 +40,48 @@ class PathCost:
 
 
 class DPNode:
-    def __init__(self, s, l, t):
+    def __init__(self, s = 0, l = 0, t = 0):
         self.s = s
         self.l = l
         self.t = t
-
-
+        self.min_cost = PathCost()
+        self.parent_pose = FrenetPose_t()
 
 
 class DPSearch:
-    planning_horizan = 0
-    planning_interval = 0
+    def __init__(self):
+        self.planning_horizon = 100
+        self.planning_interval = 10
+        self.lateral_dist = 3.0
+        self.lateral_interval = 0.5
+        self.time_interval = 1
+        self.time_horizon = 10
+        self.obstacle_list = []
+        self.reference_path = Path_t()
+        self.search_points = [[[]]]
+        self.search_map = dict()
+
+    def CollisionCheck(self, pose):
+        for obs in self.obstacle_list:
+            if obs.time == pose.time:
+                if dist(obs.pose, pose) < obs.radius:
+                    return True
+        return False
+
+    def Initialize(self):
+        init_node = DPNode(0, 0, 0)
+        self.search_map[FrenetPose_t(0, 0, 0)] = init_node
+
+        longitudinal = np.linspace(self.planning_interval, self.planning_horizon, self.planning_horizon/self.planning_interval)
+        lateral = np.linspace(-self.lateral_dist, self.lateral_dist, self.lateral_dist*2/self.lateral_interval + 1)
+        time = np.linspace(0, self.time_horizon, self.time_horizon/self.time_interval + 1)
+        for s in longitudinal:
+            for t in time:
+                for d in lateral:
+                    self.search_map[FrenetPose_t(s, d, t)] = DPNode(s, d, t)
+                    print(s, ", ", d, ", ", t)
+
+        
     # def UpdateCost(self, pre_node, cur_node, poly_curve):
 
 
@@ -57,7 +89,12 @@ class DPSearch:
 
 
     # def GenerateMinCostPath(self):
-
+dp_search = DPSearch()
+dp_search.Initialize()
+# pt = FrenetPose_t()
+# pt.s = 13
+# pt2 = FrenetPose_t(1, 2, 3)
+# print(pt.s)
 # c1 = PathCost()
 # c2 = PathCost()
 # # c3 = PathCost()
@@ -68,29 +105,29 @@ class DPSearch:
 
 
 # Test QuarticPolynomialCurve1d
-start = [0, 10, 0]
-end = [50, 0, 0]
-time_end = 8
-quartic_poly = QuinticPolynomialCurve1d(start, end, time_end)
-time = np.linspace(0, time_end, 200)
-pos = quartic_poly.Evaluate(0, time)
-vel = quartic_poly.Evaluate(1, time)
-accel = quartic_poly.Evaluate(2, time)
+# start = [0, 10, 0]
+# end = [50, 0, 0]
+# time_end = 8
+# quartic_poly = QuinticPolynomialCurve1d(start, end, time_end)
+# time = np.linspace(0, time_end, 200)
+# pos = quartic_poly.Evaluate(0, time)
+# vel = quartic_poly.Evaluate(1, time)
+# accel = quartic_poly.Evaluate(2, time)
 
-plt.figure()
-plt.subplot(311)
-plt.plot(time, pos, color = 'black')
-plt.title('Quartic Polynomial Curve')
-plt.ylabel('Position(m)')
-plt.xlim(0, time_end) 
-plt.subplot(312)
-plt.plot(time, vel, color = 'red')
-plt.ylabel('Velocity(m/s)')
-plt.xlim(0, time_end) 
-plt.subplot(313)
-plt.plot(time, accel)
-plt.xlabel('time(s)')
-plt.ylabel('Acceleration(m/s^2)')
-plt.xlim(0, time_end) 
-plt.show()
+# plt.figure()
+# plt.subplot(311)
+# plt.plot(time, pos, color = 'black')
+# plt.title('Quartic Polynomial Curve')
+# plt.ylabel('Position(m)')
+# plt.xlim(0, time_end) 
+# plt.subplot(312)
+# plt.plot(time, vel, color = 'red')
+# plt.ylabel('Velocity(m/s)')
+# plt.xlim(0, time_end) 
+# plt.subplot(313)
+# plt.plot(time, accel)
+# plt.xlabel('time(s)')
+# plt.ylabel('Acceleration(m/s^2)')
+# plt.xlim(0, time_end) 
+# plt.show()
 
